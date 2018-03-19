222e5b5daf9865ff2d2c0c666eb7668649e237df
Ottriman <gogo.gogo520@gmail.com>
2018-03-19 11:14:39 +0000

    Cooldown
    To prevent "Wall of Bullets" type abuses that could clear all the trees
    I added a cooldown mechanism for bullet shooting.

--- Assets/Scripts/ShootBolt.cs
+++ Assets/Scripts/ShootBolt.cs
@@ -6,18 +6,24 @@ public class ShootBolt : MonoBehaviour {
     public Rigidbody rb;
     public GameObject ammunition;
     public float shootForce = 100f;
+    public float cooldownTimer;
+    private float cooldownFull;
 
     // Use this for initialization
     void Start ()
     {
-		
+        cooldownFull = cooldownTimer; // To reset the cooldown
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if(0 < cooldownTimer)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
 		
-        if(Input.GetKeyDown(key))
+        if(Input.GetKeyDown(key) && cooldownTimer <= 0)
         {
             var newObject = Instantiate(ammunition, gameObject.transform); //Create the bullet
             newObject.transform.position = transform.position + -rb.transform.right;
@@ -25,7 +31,9 @@ public class ShootBolt : MonoBehaviour {
             newObject.transform.LookAt(newObject.transform, -transform.right);
             Vector3 temp = new Vector3(-1,0,0);
             newObject.GetComponent<Rigidbody>().AddForce(temp * shootForce);
-            Destroy(newObject, 3.0f);
+
+            Destroy(newObject, 3.0f); // Cleanup
+            cooldownTimer = cooldownFull; // Cooldown reset
         }
 	}
 }
